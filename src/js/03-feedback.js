10091d4a6d506649838877871e0c8dceb425d98d
Andari757 <matviyskrypka@gmail.com>
2022-04-08 00:29:28 +0300

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -3,14 +3,21 @@ const data = {
     email: "",
     textArea: ""
 };
+
 const form = document.querySelector(".feedback-form")
-form.addEventListener("input", (e) => {
-    const ContainInput = function () {        
+form.addEventListener("input", (e) => {       
         if (e.target.type === "email") { data.email = e.target.value }
-        if (e.target.type === "textarea") {data.textArea = e.target.value}
-        return data
-    }
-    localStorage.setItem("feedback-form-state", JSON.stringify(ContainInput()))
-    
-    console.log("storage:       "+localStorage.getItem("feedback-form-state"))
+        if (e.target.type === "textarea") {data.textArea = e.target.value}           
+    localStorage.setItem("feedback-form-state", JSON.stringify(data))        
 })
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    console.log(data)
+    form.reset()
+    localStorage.clear()
+})
+if (localStorage.getItem("feedback-form-state")) {
+    form[0].value = JSON.parse(localStorage.getItem("feedback-form-state")).email
+    form[1].value = JSON.parse(localStorage.getItem("feedback-form-state")).textArea
+}
+
