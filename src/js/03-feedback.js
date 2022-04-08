7eb98f192541e1cece5e7cc4bd52e3b636290e5c
Andari757 <matviyskrypka@gmail.com>
2022-04-08 10:28:35 +0300

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -3,11 +3,14 @@ const data = {
     email: "",
     textArea: ""
 };
+const containItems = function () {
+    data.email = form.elements.email.value 
+    data.textArea = form.elements.message.value 
+}
 
 const form = document.querySelector(".feedback-form")
-form.addEventListener("input", _.throttle((e) => {       
-        if (e.target.type === "email") { data.email = e.target.value }
-        if (e.target.type === "textarea") {data.textArea = e.target.value}           
+form.addEventListener("input", _.throttle(() => {    
+    containItems();
     localStorage.setItem("feedback-form-state", JSON.stringify(data))       
 },500))
 form.addEventListener("submit", (e) => {
@@ -16,8 +19,8 @@ form.addEventListener("submit", (e) => {
     form.reset()
     localStorage.clear()
 })
-if (localStorage.getItem("feedback-form-state")) {
+if (localStorage.getItem("feedback-form-state")) {   
     form[0].value = JSON.parse(localStorage.getItem("feedback-form-state")).email
     form[1].value = JSON.parse(localStorage.getItem("feedback-form-state")).textArea
 }
-
+containItems();
