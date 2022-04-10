e5697592593be14c90b2128d07404dacdfcbf638
Andari757 <matviyskrypka@gmail.com>
2022-04-10 10:25:27 +0300

    1

---
+++ src/js/02-video.js
@@ -0,0 +1,15 @@
+var throttle = require('lodash.throttle');
+// import Player from '@vimeo/player'; 
+
+ const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
+    
+
+
+player.on('timeupdate', throttle((data) => {
+    localStorage.setItem("videoplayer-current-time", data.seconds);
+    console.log(data)    
+}, 1000) );
+if (localStorage.getItem("videoplayer-current-time")) {
+    player.setCurrentTime(localStorage.getItem("videoplayer-current-time") )
+}
